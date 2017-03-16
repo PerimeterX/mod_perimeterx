a7eab1a806285383de49c3454e3344a273754ba7
Aviad Shikloshi <aviad@perimeterx.com>
2017-03-16 15:08:29 +0200

    run test script

--- run-test.sh
+++ run-test.sh
@@ -3,4 +3,4 @@
 sed -i 's@APP_ID@'"$APP_ID"'@' /home/r/mod_perimeterx/t/conf/extra.conf.in
 sed -i 's@TOKEN@'"$AUTH_TOKEN"'@' /home/r/mod_perimeterx/t/conf/extra.conf.in
 
-./home/r/mod_perimeterx/t/TEST -v
+/home/r/mod_perimeterx/t/TEST -v
